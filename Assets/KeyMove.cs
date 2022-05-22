744e2431417c48d91082363381f7d77b231a3b42
lkh1945 <lkh_1945@duam.net>
2022-05-22 17:06:31 +0900

    1version

---
+++ Assets/KeyMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyMove : MonoBehaviour
+{
+    // 키버튼을 이용해서 움직이자.
+    Vector3 pos;
+    public float speed;
+    private void Start()
+    {
+        pos = transform.position;
+        speed = 1;
+    }
+    void Update()
+    {
+        pos.x += speed * Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+        pos.y += speed * Input.GetAxisRaw("Vertical") * Time.deltaTime;
+        transform.position = pos;
+    }
+}
